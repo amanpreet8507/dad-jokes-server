c31830a436b02e28992e1a36c8a9b96aa07c8fbc
ergaganjit <er.gaganjit.kaur@gmail.com>
2024-02-02 01:56:42 +0530

    Add endpoint to get random joke

--- routes/jokes.js
+++ routes/jokes.js
@@ -3,32 +3,35 @@ const express = require('express')
 const router = express.Router();
 const jokesData = require("../data/jokes.json");
 
-router.get('/jokes', (req, res) => {
-    res.json(jokesData);
-  });
 router.get('/', (req, res) => {
-    let NumAllJokes = jokesData.length;
-    const randomNum = Math.floor(Math.random() * NumAllJokes + 1);
-    console.log(randomNum);
-    const joke = jokesData.find(j => j.id === randomNum);
-  
-    if (!joke) {
-      res.status(404).json({ message: 'Joke not found' });
-    } else {
-      res.json(joke);
-    }
+  res.json(jokesData);
+});
 
-  });
-  
-  router.get('/:id', (req, res) => {
-    const jokeId = parseInt(req.params.id);
-    const joke = jokesData.find(j => j.id === jokeId);
-  
-    if (!joke) {
-      res.status(404).json({ message: 'Joke not found' });
-    } else {
-      res.json(joke);
-    }
-  });
+router.get('/random', (req, res) => {
+  // Get the length of array storing all jokes
+  let numAllJokes = jokesData.length;
+  // Get a random number between 1 and numAllJokes
+  const randomNum = Math.floor(Math.random() * (numAllJokes-1) + 1);
+  // Return the random joke that has same id as randomNum
+  const joke = jokesData.find(j => j.id === randomNum);
+
+  if (!joke) {
+    res.status(404).json({ message: 'Joke not found' });
+  } else {
+    res.json(joke);
+  }
+
+});
+
+router.get('/:id', (req, res) => {
+  const jokeId = parseInt(req.params.id);
+  const joke = jokesData.find(j => j.id === jokeId);
+
+  if (!joke) {
+    res.status(404).json({ message: 'Joke not found' });
+  } else {
+    res.json(joke);
+  }
+});
 
 module.exports=router;
