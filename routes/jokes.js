db93a69be259b504d67f3d13571cc9b0df30f9a5
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-03-21 17:38:27 -0700

    error handle if comments arr does ot exist

--- routes/jokes.js
+++ routes/jokes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const jokesData = require("../data/jokes.json");
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 
 router.get("/", (req, res) => {
   res.json(jokesData);
 });
 
+// To Get random joke
 router.get("/random", (req, res) => {
   // Get the length of array storing all jokes
   let numAllJokes = jokesData.length;
@@ -34,35 +35,55 @@ router.get("/:id", (req, res) => {
   }
 });
 
-// Endpoint to handle POST requests
-router.post("/:id/comments", (req, res) => {
-  // Access the JSON data sent by the client
-  const { name, comment } = req.body;
+// Endpoint to handle GET, POST comments
+router
+  .route("/:id/comments")
+  .get((req, res) => {
+    const jokeId = parseInt(req.params.id);
+    const joke = jokesData.find((j) => j.id === jokeId);
 
-  if (!name || !comment)
-    return res.status(400).json("Please input name and comment");
-  const jokeId = req.params.id;
+    if (!joke) {
+      res.status(404).json({ message: "Joke not found" });
+    } else {
+      res.json(joke.comments || []);
+    }
+  })
+  .post((req, res) => {
+    // Access the JSON data sent by the client
+    const { name, comment } = req.body;
+    const jokeId = parseInt(req.params.id);
 
-  // read file
-  const newCommentsList = JSON.parse(fs.readFileSync("./data/jokes.json"));
+    if (!name || !comment)
+      return res.status(400).json("Please input name and comment");
 
-  //Find the joke
-  const joke = newCommentsList.find((joke) => jokeId === jokeId);
-  if (!joke) {
-    return res.status(404).json("joke not found");
-  }
+    // read file
+    const newCommentsList = JSON.parse(fs.readFileSync("./data/jokes.json"));
 
-  const newComment = {
-    id: uuidv4(),
-    name,
-    comment,
-    likes:0,
-    timestamp: Date.now(),
-  };
-  joke.comments.push(newComment);
+    // Find the joke
+    const jokeIndex = newCommentsList.findIndex((joke) => joke.id === jokeId); // Changed variable name to avoid confusion
+    console.log(jokeIndex , 'jokeIndex')
+    if (jokeIndex == -1) {
+      return res.status(404).json("Joke not found");
+    }
 
-  fs.writeFileSync("./data/jokes.json", JSON.stringify(newCommentsList));
-  res.status(201).json(joke);
-});
+    const joke = newCommentsList[jokeIndex];
+    if (!joke.comments) {
+      joke.comments = []; // Create comments array if it doesn't exist
+    }
+
+    const newComment = {
+      id: uuidv4(),
+      name,
+      comment,
+      likes: 0,
+      timestamp: Date.now(),
+    };
+    joke.comments.push(newComment);
+
+    newCommentsList[jokeIndex] = joke;
+
+    fs.writeFileSync("./data/jokes.json", JSON.stringify(newCommentsList));
+    res.status(201).json(joke);
+  });
 
 module.exports = router;
